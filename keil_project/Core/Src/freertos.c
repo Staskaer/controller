05423e50640f822d93c5122be9e2bf159e1d8aee
Staskaer <liujiaxin011121@gmail.com>
2023-11-11 11:41:29 +0800

    change priority

--- keil_project/Core/Src/freertos.c
+++ keil_project/Core/Src/freertos.c
@@ -33,7 +33,6 @@
 #include "OLED.h"   
 #include <stdlib.h>
 #include <string.h>
-#include "user.h"
 
 /* USER CODE END Includes */
 
@@ -212,9 +211,37 @@ osSemaphoreId iic_semHandle;
 
 // 全局变量，使用信号量来进行控制
 
-struct Data global_data={0};
+struct Data
+{
+// ADC采样数据，x1、y1是左手柄的x、y值，摇杆在中间时，两者均为2048左右
+// x2、y2是右手柄的x、y值，摇杆在中间时，两者均为2048左右
+// b1 b2是顶部左右两个电位器，电位器向中间旋转时，数值会增大
+uint16_t g_x1, g_x2, g_y1, g_y2, g_b1, g_b2;
+
+// 一共8+2+3的读取，用16位来表示，仅使用后13位
+// 后八位为key_left和key_right的表示
+// 前5位为switch和顶部key的表示
+uint16_t g_key_result;
+
+// 表示位置和当前的x、y方向上的角度
+// pos_x的范围为[420,850] pos_y的范围为[320, 610]
+// 角度x的范围为(-50, +50) 角度y的范围为(-80, +80)
+float g_pos_x,g_pos_y;
+float g_x,g_y;
+
+} global_data={0};
 
 
+void trans_func(struct Data d)
+{
+	
+	if(d.g_y1<1000)
+		printf("w");
+	else if(d.g_x1<1000)
+		printf("a");
+	else if(d.g_x1>3900)
+		printf("d");
+}
 
 /* USER CODE END FunctionPrototypes */
 
@@ -286,15 +313,15 @@ void MX_FREERTOS_Init(void) {
   miantaskHandle = osThreadCreate(osThread(miantask), NULL);
 
   /* definition and creation of ADCtask */
-  osThreadDef(ADCtask, ADC_func, osPriorityIdle, 0, 128);
+  osThreadDef(ADCtask, ADC_func, osPriorityNormal, 0, 128);
   ADCtaskHandle = osThreadCreate(osThread(ADCtask), NULL);
 
   /* definition and creation of keytask */
-  osThreadDef(keytask, key_func, osPriorityIdle, 0, 128);
+  osThreadDef(keytask, key_func, osPriorityNormal, 0, 128);
   keytaskHandle = osThreadCreate(osThread(keytask), NULL);
 
   /* definition and creation of iictask */
-  osThreadDef(iictask, iic_func, osPriorityIdle, 0, 128);
+  osThreadDef(iictask, iic_func, osPriorityNormal, 0, 128);
   iictaskHandle = osThreadCreate(osThread(iictask), NULL);
 
   /* USER CODE BEGIN RTOS_THREADS */
@@ -357,7 +384,6 @@ void main_func(void const * argument)
 	draw_key_line(21+85,23,DFLAG);
 	osDelay(100);
 	
-	trans_init();
 	
   /* Infinite loop */
   for(;;)
